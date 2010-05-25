73a057df181e41742b29cc95f407ac56cd30b71e
apmeneel <andy.meneely@gmail.com>
2010-05-25 13:32:35 -0400

    made all of the usernames emails

--- sql/testdata.sql
+++ sql/testdata.sql
@@ -2,29 +2,29 @@ DELETE FROM Users;
 DELETE FROM Distance;
 
 INSERT INTO Users(User,Password,Role,Name,Project) VALUES
-('apmeneel', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Andy Programmer', 'Developer Survey'),
-('carter', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Carter', 'Developer Survey'),
-('hogan', '6f1ed002ab5595859014ebf0951522d9', 'user','Col. Hogan', 'Developer Survey'),
-('kinch', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Sgt. Kinchloe', 'Developer Survey'),
-('lebeau', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Lebeau', 'Developer Survey'),
-('newkirk', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Newkirk', 'Developer Survey')
+('apmeneel@andymeneely.com', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Andy Programmer', 'Developer Survey'),
+('carter@andymeneely.com', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Carter', 'Developer Survey'),
+('hogan@andymeneely.com', '6f1ed002ab5595859014ebf0951522d9', 'user','Col. Hogan', 'Developer Survey'),
+('kinch@andymeneely.com', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Sgt. Kinchloe', 'Developer Survey'),
+('lebeau@andymeneely.com', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Lebeau', 'Developer Survey'),
+('newkirk@andymeneely.com', '6f1ed002ab5595859014ebf0951522d9', 'user', 'Newkirk', 'Developer Survey')
 ;
 
 INSERT INTO Distance(User1, User2, Distance) VALUES
-('apmeneel', 'hogan', '1.2'),
-('apmeneel', 'carter', '5.2'),
-('apmeneel', 'lebeau', '5.2'),
-('apmeneel', 'newkirk', '6.34'),
-('apmeneel', 'kinch', '3.24'),
-('carter', 'hogan', '10.4'),
-('carter', 'kinch', '37.33'),
-('carter', 'lebeau', '37.33'),
-('carter', 'newkirk', '30.1'),
-('hogan', 'kinch', '15.1'),
-('hogan', 'lebeau', '37.33'),
-('hogan', 'newkirk', '35.0'),
-('kinch', 'lebeau', '50'),
-('lebeau', 'newkirk', '50')
+('apmeneel@andymeneely.com', 'hogan@andymeneely.com', '1.2'),
+('apmeneel@andymeneely.com', 'carter@andymeneely.com', '5.2'),
+('apmeneel@andymeneely.com', 'lebeau@andymeneely.com', '5.2'),
+('apmeneel@andymeneely.com', 'newkirk@andymeneely.com', '6.34'),
+('apmeneel@andymeneely.com', 'kinch@andymeneely.com', '3.24'),
+('carter@andymeneely.com', 'hogan@andymeneely.com', '10.4'),
+('carter@andymeneely.com', 'kinch@andymeneely.com', '37.33'),
+('carter@andymeneely.com', 'lebeau@andymeneely.com', '37.33'),
+('carter@andymeneely.com', 'newkirk@andymeneely.com', '30.1'),
+('hogan@andymeneely.com', 'kinch@andymeneely.com', '15.1'),
+('hogan@andymeneely.com', 'lebeau@andymeneely.com', '37.33'),
+('hogan@andymeneely.com', 'newkirk@andymeneely.com', '35.0'),
+('kinch@andymeneely.com', 'lebeau@andymeneely.com', '50'),
+('lebeau@andymeneely.com', 'newkirk@andymeneely.com', '50')
 ;
 
 
