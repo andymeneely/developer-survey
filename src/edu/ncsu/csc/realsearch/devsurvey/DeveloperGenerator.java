ef91a005949158bacd1f125dec737811051a128c
apmeneel <andy.meneely@gmail.com>
2010-06-02 10:28:02 -0800

    re-did the developer generator so that it samples different distances

--- src/edu/ncsu/csc/realsearch/devsurvey/DeveloperGenerator.java
+++ src/edu/ncsu/csc/realsearch/devsurvey/DeveloperGenerator.java
@@ -1,67 +1,52 @@
 package edu.ncsu.csc.realsearch.devsurvey;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.ResourceBundle;
 
 public class DeveloperGenerator {
-	private static Random rnd = new Random();
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(DeveloperGenerator.class);
+	private int choppedListSize;
+	private DeveloperDAO devDAO;
 
-	//TODO Redo this so it's statistically better.
+	public DeveloperGenerator() {
+		choppedListSize = getListSize();
+		devDAO = new DeveloperDAO();
+	}
+
+	public DeveloperGenerator(int finalListSize, DeveloperDAO devDAO) {
+		this.choppedListSize = finalListSize;
+		this.devDAO = devDAO;
+	}
+
 	public List<Developer> getDevelopers(String username) throws SQLException {
-		Connection conn = null;
-		ResultSet rs = null;
-		PreparedStatement ps = null;
-		List<Developer> list = new ArrayList<Developer>(50);
-		try {
-			conn = DBConnector.getInstance().getConnection();
-			ps = conn.prepareStatement("SELECT * FROM AllDistances "
-					+ "WHERE User1=? OR User2=? ORDER BY Distance ASC");
-			ps.setString(1, username);
-			ps.setString(2, username);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				Developer dev = new Developer();
-				String user1 = rs.getString("user1");
-				String user2 = rs.getString("user2");
-				dev.setUsername(username.equals(user2) ? user1 : user2); // the OTHER one
-				dev.setDistanceTo(rs.getDouble("distance"));
-				list.add(dev);
-			}
-			return fillInNames(chop(list));
-		} finally {
-			if (rs != null)
-				rs.close();
-			if (ps != null)
-				ps.close();
-			if (conn != null)
-				conn.close();
-		}
+		List<Developer> list = devDAO.getAllDeveloperDistances(username);
+		return fillInNames(chop(list));
 	}
 
 	private List<Developer> fillInNames(List<Developer> fillIn) throws SQLException {
-		for (Developer dev: fillIn) {
-			Developer fullDev = new DeveloperDAO().getDeveloper(dev.getUsername());
+		for (Developer dev : fillIn) {
+			Developer fullDev = devDAO.getDeveloper(dev.getUsername());
 			dev.setName(fullDev.getName());
 		}
 		return fillIn;
 	}
 
 	private List<Developer> chop(List<Developer> list) {
-		int choppedListSize = getListSize();
-		if (list.size() < 2 || list.size() <= choppedListSize)
+		if (list.size() < 2 || list.size() <= choppedListSize){
+			log.debug("Going with the whole list.");
 			return list;
-		int numRemoves = list.size() - choppedListSize;
-		for (int i = 0; i < numRemoves; i++) {
-			int index = rnd.nextInt(list.size());
-			list.remove(index);
 		}
-		return list;
+
+		List<Developer> choppedList = new ArrayList<Developer>(choppedListSize);
+		choppedList.add(list.get(0));
+		double chunk = list.size() / (choppedListSize - 1);
+		for (int i = 1; i <= choppedListSize - 2; i++) {
+			choppedList.add(list.get((int) (chunk * i)));
+		}
+		choppedList.add(list.get(list.size() - 1));
+		return choppedList;
 	}
 
 	private int getListSize() {
@@ -72,6 +57,7 @@ public class DeveloperGenerator {
 			size = size > 2 ? size : 2;
 		} catch (Throwable t) {// problem? oh well - go with default.
 		}
+		log.info("Number of developers shown: " + size);
 		return size;
 	}
 }
