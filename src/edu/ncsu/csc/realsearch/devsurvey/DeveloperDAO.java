19db4008ea86f077ce49a93f97ebbf48f4d851a3
apmeneel <andy.meneely@gmail.com>
2010-06-02 14:17:06 -0400

    added a new call to developer DAO for better design and testing

--- src/edu/ncsu/csc/realsearch/devsurvey/DeveloperDAO.java
+++ src/edu/ncsu/csc/realsearch/devsurvey/DeveloperDAO.java
@@ -9,6 +9,37 @@ import java.util.List;
 
 public class DeveloperDAO {
 
+	public List<Developer> getAllDeveloperDistances(String username) throws SQLException{
+		Connection conn = null;
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+		List<Developer> list = new ArrayList<Developer>(50);
+		try {
+			conn = DBConnector.getInstance().getConnection();
+			ps = conn.prepareStatement("SELECT * FROM AllDistances "
+					+ "WHERE User1=? OR User2=? ORDER BY Distance ASC");
+			ps.setString(1, username);
+			ps.setString(2, username);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				Developer dev = new Developer();
+				String user1 = rs.getString("user1");
+				String user2 = rs.getString("user2");
+				dev.setUsername(username.equals(user2) ? user1 : user2); // the OTHER one
+				dev.setDistanceTo(rs.getDouble("distance"));
+				list.add(dev);
+			}
+			return list;
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (ps != null)
+				ps.close();
+			if (conn != null)
+				conn.close();
+		}
+	}
+	
 	public Developer getDeveloper(String username) throws SQLException {
 		Connection conn = null;
 		ResultSet rs = null;
