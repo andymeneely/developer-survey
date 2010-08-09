fa453dbb3cd1748a1e0d24e0baccced0f0c7d1db
apmeneel <andy.meneely@gmail.com>
2010-08-09 16:38:37 -0400

    some debugging lines for sanity

--- stats/question3.R
+++ stats/question3.R
@@ -14,12 +14,14 @@ for(i in seq(1:numRespondants) ) {
 	perceivedDistance <- question3$PerceivedDistance[question3$RespondingUser == users$RespondingUser[i]]
 	weightedDNDistance <- question3$WeightedDNDistance[question3$RespondingUser == users$RespondingUser[i]]
 	unweightedDNDistance <- question3$UnweightedDNDistance[question3$RespondingUser == users$RespondingUser[i]]
-	print("-----------")
-	print(unweightedDNDistance)
-	print(perceivedDistance)
 	results$rhoWeighted[i] <- cor(perceivedDistance, weightedDNDistance, method="spearman")
 	results$rhoUnWeighted[i] <- cor(perceivedDistance, unweightedDNDistance, method="spearman")
 	#results$project[i] <- question3$Project[question3$RespondingUser == users$RespondingUser[i]]
+
+	print("-----------")
+	print(unweightedDNDistance)
+	print(perceivedDistance)
+	print(cor(perceivedDistance, unweightedDNDistance, method="spearman"))
 }
 #Avg of Spearmans for Weighted Distances
 summary(results$rhoWeighted)
